ef4c00f5ef4868e9296204048b27a54b19564688
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2019-04-03 10:30:42 +0700

    add more types

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,24 +3,3 @@ interface ObjectIndexer<T> {
   [index: string]: T;
   [index: number]: T;
 }
-
-/** redux interfaces */
-interface Data {
-  name: string;
-  value: Buffer;
-}
-
-interface State {
-  data: Data[] | null;
-}
-
-// redux actions
-interface Action<T> {
-  type: string;
-  payload: T;
-  error?: boolean;
-}
-
-interface ActionStoreZipData {
-  data: Data[];
-}
