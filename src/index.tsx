e5cef29c7b81fb4c1c607de0aa703d9f20cc7f3e
Bagas Wahyu Hidayah <bagasdotid69@gmail.com>
2019-04-03 10:30:28 +0700

    populate list of data on startup, add Redux Store Provider for root component

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/store';
+import { getDataList } from './helpers/localstorage';
+import { populateDataList } from './redux/actions/actions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+getDataList().then(list => {
+  store.dispatch(populateDataList(list));
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
